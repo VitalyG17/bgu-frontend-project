2e5cb700c5a35059c87bb0bcc9b0a4cf6e50fc34
EvkaZelenskaya <150838007+EvkaZelenskaya@users.noreply.github.com>
2024-12-23 02:29:29 +0300

    Config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,15 @@
 import pluginJs from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
-import pluginReact from 'eslint-plugin-react';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReactConfig,
   prettierConfig,
 
   {
